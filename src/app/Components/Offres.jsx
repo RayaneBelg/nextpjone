3de8296ab55b37e5038b834677bdc578a5947dda
unknown <belgasmi.rayane1@gmail.com>
2024-12-19 20:10:55 +0100

    hhghjk

--- src/app/Components/Offres.jsx
+++ src/app/Components/Offres.jsx
@@ -24,7 +24,7 @@ export default function Offres() {
           className="flex-shrink-0 w-full origin-top"
         >
           <video
-            className="h-[25vh] md:h-[35vh] lg:h-[40vh] w-full object-cover object-bottom"
+            className="h-[25vh] sm:h-[30vh] md:h-[35vh] lg:h-[40vh] w-full object-cover object-bottom"
             src="../../head.mov"
             autoPlay
             loop
@@ -42,7 +42,7 @@ export default function Offres() {
           </h1>
 
           {/* Grid of Cards */}
-          <motion.div
+           {/* Grid of Cards <motion.div
             ref={cardsRef}
             initial={{ opacity: 0 }}
             animate={isCardsInView ? { opacity: 1 } : {}}
@@ -78,7 +78,47 @@ export default function Offres() {
               </p>
               </motion.div>
             ))}
-          </motion.div>
+          </motion.div> */}
+           <motion.div
+                    ref={cardsRef}
+                    initial={{ opacity: 0 }}
+                    animate={isCardsInView ? { opacity: 1 } : {}}
+                    transition={{
+                      duration: 0.8,
+                      staggerChildren: 0.2, // Stagger animation for child elements
+                    }}
+                    className="flex  snap-x snap-mandatory overflow-x-auto gap-4 w-full lg:justify-center p-4"
+                  >
+                    {offers.map((off, id) => (
+                      <motion.div
+                        key={id}
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={isCardsInView ? { opacity: 1, y: 0 } : {}}
+                        transition={{ duration: 0.5, delay: id * 0.2 }}
+                        className="snap-center card flex-shrink-0 w-80 border border-gray-200 shadow-md text-center bg-white hover:shadow-lg transition-shadow p-4"
+                      >
+                        <h1 className="font-westmeath uppercase text-xl sm:text-3xl  text-center my-2">
+                          {off.nom}
+                        </h1>
+                        <ul className="space-y-3 sm:space-y-4 sm:my-8 font-mono">
+                          {off.inclus.map((item, idx) => (
+                            <li
+                              key={idx}
+                              className="text-xs font-bold sm:uppercase text-gray-700 "
+                            >
+                              {item}
+                            </li>
+                          ))}
+                        </ul>
+                        <p className="text-sm sm:text-2xl mt-4 font-extrabold text-red-700 xl:text-3xl">
+                          {off.prix} €
+                        </p>
+                      </motion.div>
+                    ))}
+                  </motion.div>
+
+
+          
         </div>
       </div>
     </div>
